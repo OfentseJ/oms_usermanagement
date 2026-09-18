24e83ad540a96aec9888ce3d9d4175d62e0b3d3a
Ofentse <ofentsemakhutja@icloud.com>
2026-09-18 10:42:00 +0200

    generating the secret key

--- src/main/java/com/fnb/usermanagement/security/serviceImpl/JwtServiceImpl.java
+++ src/main/java/com/fnb/usermanagement/security/serviceImpl/JwtServiceImpl.java
@@ -2,13 +2,41 @@ package com.fnb.usermanagement.security.serviceImpl;
 
 import com.fnb.usermanagement.entity.User;
 import com.fnb.usermanagement.security.JwtService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Service
 public class JwtServiceImpl implements JwtService {
+
+    @Value("${secret}")
+    private String secret;
+
+    @Value("${expiration-ms}")
+    private Long expirationMs;
+
+    private SecretKey secretKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
     @Override
     public String generateToken(User user) {
-        return "";
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationMs);
+
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("customerId", user.getCustomerId())
+                .claim("role", user.getRole().name())
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(secretKey())
+                .compact();
     }
 
     @Override
